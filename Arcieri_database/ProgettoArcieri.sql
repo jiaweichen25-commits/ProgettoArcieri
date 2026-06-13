f95b3c3a395e51da3abff342d79062f96d04926a
jiawei chen <jiawei.chen25@stud.itsaltoadriatico.it>
2026-06-14 01:51:40 +0200

    aggiunto Tpianogare e TSpianogare all db

--- Arcieri_database/ProgettoArcieri.sql
+++ Arcieri_database/ProgettoArcieri.sql
@@ -2,23 +2,23 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS public."Tutenti"
 (
-    "IDutente" serial NOT NULL,
-    "E-mail" character varying NOT NULL UNIQUE,
-    passwd_hash character varying NOT NULL,
-    "Ruolo" character varying NOT NULL,
-    creato_il timestamp without time zone DEFAULT NOW(),
+    "IDutente"          serial NOT NULL,
+    "E-mail"            character varying NOT NULL UNIQUE,
+    passwd_hash         character varying NOT NULL,
+    "Ruolo"             character varying NOT NULL,
+    creato_il           timestamp without time zone DEFAULT NOW(),
     PRIMARY KEY ("IDutente")
 );
 
 CREATE TABLE IF NOT EXISTS public."Tistruttori"
 (
-    "IDistruttore" serial NOT NULL,
-    "IDutente" integer NOT NULL,
-    "Nome" character varying NOT NULL,
-    "Cognome" character varying NOT NULL,
-    "Qualifica" character varying,
-    "Cellulare" character varying,
-    "E-mail" character varying,
+    "IDistruttore"      serial NOT NULL,
+    "IDutente"          integer NOT NULL,
+    "Nome"              character varying NOT NULL,
+    "Cognome"           character varying NOT NULL,
+    "Qualifica"         character varying,
+    "Cellulare"         character varying,
+    "E-mail"            character varying,
     PRIMARY KEY ("IDistruttore")
 );
 
@@ -27,74 +27,74 @@ COMMENT ON TABLE public."Tistruttori"
 
 CREATE TABLE IF NOT EXISTS public."Tatleti"
 (
-    "IDatleta" serial NOT NULL,
-    "IDistruttore" integer NOT NULL,
-    "IDutente" integer NOT NULL,
-    "Nome" character varying NOT NULL,
-    "Cognome" character varying NOT NULL,
-    "Indirizzo" character varying,
-    "CAP" character varying,
-    "CITTA" character varying,
-    "Codice_Fiscale" character varying NOT NULL UNIQUE,
-    "Telefono" character varying,
-    "Cellulare" character varying,
-    "E-mail" character varying,
-    "DataNascita" date,
-    "Foto" text,
+    "IDatleta"              serial NOT NULL,
+    "IDistruttore"          integer NOT NULL,
+    "IDutente"              integer NOT NULL,
+    "Nome"                  character varying NOT NULL,
+    "Cognome"               character varying NOT NULL,
+    "Indirizzo"             character varying,
+    "CAP"                   character varying,
+    "CITTA"                 character varying,
+    "Codice_Fiscale"        character varying NOT NULL UNIQUE,
+    "Telefono"              character varying,
+    "Cellulare"             character varying,
+    "E-mail"                character varying,
+    "DataNascita"           date,
+    "Foto"                  text,
     PRIMARY KEY ("IDatleta")
 );
 
 CREATE TABLE IF NOT EXISTS public."Tmateriali"
 (
-    "IDmateriale" serial NOT NULL,
-    "IDatleta" integer NOT NULL,
-    "Riser" character varying,
-    "LunghezzaRiser" character varying,
-    "Flettenti" character varying,
-    "LunghezzaFlettenti" character varying,
-    "PotenzaNominale" character varying,
-    "Rest" character varying,
-    "Mirino" character varying,
-    "Stabilizzazione" character varying,
-    "Aste" character varying,
-    "LunghezzaAste" character varying,
-    "Punte" character varying,
-    "PesoPunte" character varying,
-    "Cocche" character varying,
-    "Alette" character varying,
-    "LunghezzaAlette" character varying,
-    "Spine" character varying,
-    "Corda" character varying,
-    "Fili" integer,
-    "Patella" character varying,
-    "Bottone" character varying,
-    "Molla" character varying,
-    "TillerSuperiore" character varying,
-    "TillerInferiore" character varying,
-    "Brace" character varying,
-    "Allungo" character varying,
-    "PotenzaReale" character varying,
-    "PuntoIncocco" character varying,
-    "Data" date NOT NULL,
-    "MaterialeCorrente" boolean NOT NULL,
+    "IDmateriale"           serial NOT NULL,
+    "IDatleta"              integer NOT NULL,
+    "Riser"                 character varying,
+    "LunghezzaRiser"        character varying,
+    "Flettenti"             character varying,
+    "LunghezzaFlettenti"    character varying,
+    "PotenzaNominale"       character varying,
+    "Rest"                  character varying,
+    "Mirino"                character varying,
+    "Stabilizzazione"       character varying,
+    "Aste"                  character varying,
+    "LunghezzaAste"         character varying,
+    "Punte"                 character varying,
+    "PesoPunte"             character varying,
+    "Cocche"                character varying,
+    "Alette"                character varying,
+    "LunghezzaAlette"       character varying,
+    "Spine"                 character varying,
+    "Corda"                 character varying,
+    "Fili"                  integer,
+    "Patella"               character varying,
+    "Bottone"               character varying,
+    "Molla"                 character varying,
+    "TillerSuperiore"       character varying,
+    "TillerInferiore"       character varying,
+    "Brace"                 character varying,
+    "Allungo"               character varying,
+    "PotenzaReale"          character varying,
+    "PuntoIncocco"          character varying,
+    "Data"                  date NOT NULL,
+    "MaterialeCorrente"     boolean NOT NULL,
     PRIMARY KEY ("IDmateriale")
 );
 
 CREATE TABLE IF NOT EXISTS public."Tallenamenti"
 (
-    "IDallenamento" serial NOT NULL,
-    "IDatleta" integer NOT NULL,
-    "DataInizio" date NOT NULL,
-    "DataFine" date NOT NULL,
-    "Obiettivi" character varying,
+    "IDallenamento"         serial NOT NULL,
+    "IDatleta"              integer NOT NULL,
+    "DataInizio"            date NOT NULL,
+    "DataFine"              date NOT NULL,
+    "Obiettivi"             character varying,
     PRIMARY KEY ("IDallenamento")
 );
 
 CREATE TABLE IF NOT EXISTS public."TdetAllenamenti"
 (
-    "IDallenamento" integer NOT NULL,
-    "IDsettimana" integer NOT NULL,
-    "IDseduta" integer NOT NULL,
+    "IDallenamento"         integer NOT NULL,
+    "IDsettimana"           integer NOT NULL,
+    "IDseduta"              integer NOT NULL,
     PRIMARY KEY ("IDallenamento", "IDsettimana", "IDseduta")
 );
 
@@ -103,26 +103,26 @@ COMMENT ON TABLE public."TdetAllenamenti"
 
 CREATE TABLE IF NOT EXISTS public."TdetStretching"
 (
-    "IDdetStretching" serial NOT NULL,
-    "IDallenamento" integer NOT NULL,
-    "IDsettimana" integer NOT NULL,
-    "IDseduta" integer NOT NULL,
-    "Lunedi" character varying,
-    "Martedi" character varying,
-    "Mercoledi" character varying,
-    "Giovedi" character varying,
-    "Venerdi" character varying,
-    "Sabato" character varying,
-    "Domenica" character varying,
-    "IDesercizioStretching" integer,
+    "IDdetStretching"           serial NOT NULL,
+    "IDallenamento"             integer NOT NULL,
+    "IDsettimana"               integer NOT NULL,
+    "IDseduta"                  integer NOT NULL,
+    "Lunedi"                    character varying,
+    "Martedi"                   character varying,
+    "Mercoledi"                 character varying,
+    "Giovedi"                   character varying,
+    "Venerdi"                   character varying,
+    "Sabato"                    character varying,
+    "Domenica"                  character varying,
+    "IDesercizioStretching"     integer,
     PRIMARY KEY ("IDdetStretching")
 );
 
 CREATE TABLE IF NOT EXISTS public."TSstretching"
 (
-    "IDesercizioStretching" serial NOT NULL,
-    "NomeEsercizio" character varying,
-    "Descrizione" text,
+    "IDesercizioStretching"     serial NOT NULL,
+    "NomeEsercizio"             character varying,
+    "Descrizione"               text,
     PRIMARY KEY ("IDesercizioStretching")
 );
 
@@ -131,46 +131,46 @@ COMMENT ON TABLE public."TSstretching"
 
 CREATE TABLE IF NOT EXISTS public."TdetRiscaldamento"
 (
-    "IDdetRiscaldamento" serial NOT NULL,
-    "IDallenamento" integer NOT NULL,
-    "IDsettimana" integer NOT NULL,
-    "IDseduta" integer NOT NULL,
-    "Lunedi" character varying,
-    "Martedi" character varying,
-    "Mercoledi" character varying,
-    "Giovedi" character varying,
-    "Venerdi" character varying,
-    "Sabato" character varying,
-    "Domenica" character varying,
-    "IDesercizioRiscaldamento" integer,
+    "IDdetRiscaldamento"        serial NOT NULL,
+    "IDallenamento"             integer NOT NULL,
+    "IDsettimana"               integer NOT NULL,
+    "IDseduta"                  integer NOT NULL,
+    "Lunedi"                    character varying,
+    "Martedi"                   character varying,
+    "Mercoledi"                 character varying,
+    "Giovedi"                   character varying,
+    "Venerdi"                   character varying,
+    "Sabato"                    character varying,
+    "Domenica"                  character varying,
+    "IDesercizioRiscaldamento"  integer,
     PRIMARY KEY ("IDdetRiscaldamento")
 );
 
 CREATE TABLE IF NOT EXISTS public."TSriscaldamento"
 (
-    "IDesercizioRiscaldamento" serial NOT NULL,
-    "NomeEsercizio" character varying,
-    "DescrizioneEsercizio" text,
+    "IDesercizioRiscaldamento"      serial NOT NULL,
+    "NomeEsercizio"                 character varying,
+    "DescrizioneEsercizio"          text,
     PRIMARY KEY ("IDesercizioRiscaldamento")
 );
 
 CREATE TABLE IF NOT EXISTS public."TdetTecForCor"
 (
-    "IDdetTecForCor" serial NOT NULL,
-    "IDallenamento" integer NOT NULL,
-    "IDsettimana" integer NOT NULL,
-    "IDseduta" integer NOT NULL,
-    "PosizionePiedi" character varying,
-    "Lunedi" character varying,
-    "Martedi" character varying,
-    "Mercoledi" character varying,
-    "Giovedi" character varying,
-    "Venerdi" character varying,
-    "Sabato" character varying,
-    "Domenica" character varying,
-    "IDdistanza" integer,
-    "IDtarga" integer,
-    "IDdescrizioneEsercizio" integer,
+    "IDdetTecForCor"            serial NOT NULL,
+    "IDallenamento"             integer NOT NULL,
+    "IDsettimana"               integer NOT NULL,
+    "IDseduta"                  integer NOT NULL,
+    "PosizionePiedi"            character varying,
+    "Lunedi"                    character varying,
+    "Martedi"                   character varying,
+    "Mercoledi"                 character varying,
+    "Giovedi"                   character varying,
+    "Venerdi"                   character varying,
+    "Sabato"                    character varying,
+    "Domenica"                  character varying,
+    "IDdistanza"                integer,
+    "IDtarga"                   integer,
+    "IDdescrizioneEsercizio"    integer,
     PRIMARY KEY ("IDdetTecForCor")
 );
 
@@ -179,100 +179,100 @@ COMMENT ON TABLE public."TdetTecForCor"
 
 CREATE TABLE IF NOT EXISTS public."TStarga"
 (
-    "IDtarga" serial NOT NULL,
-    "NomeTarga" character varying,
-    "Descrizione" text,
+    "IDtarga"           serial NOT NULL,
+    "NomeTarga"         character varying,
+    "Descrizione"       text,
     PRIMARY KEY ("IDtarga")
 );
 
 CREATE TABLE IF NOT EXISTS public."TSdistanza"
 (
-    "IDdistanza" serial NOT NULL,
-    "NomeEsercizio" character varying,
-    "Descrizione" character varying,
+    "IDdistanza"            serial NOT NULL,
+    "NomeEsercizio"         character varying,
+    "Descrizione"           character varying,
     PRIMARY KEY ("IDdistanza")
 );
 
 CREATE TABLE IF NOT EXISTS public."TSDescrizioneEsercizio"
 (
-    "IDdescrizioneEsercizio" serial NOT NULL,
-    "NomeEsercizio" character varying,
-    "Descrizione" text,
+    "IDdescrizioneEsercizio"        serial NOT NULL,
+    "NomeEsercizio"                 character varying,
+    "Descrizione"                   text,
     PRIMARY KEY ("IDdescrizioneEsercizio") 
 );
 
 CREATE TABLE IF NOT EXISTS public."TdetAllFisForRes"
 (
-    "IDdetAllFisForRes" serial NOT NULL,
-    "IDallenamento" integer NOT NULL,
-    "IDsettimana" integer NOT NULL,
-    "IDseduta" integer NOT NULL,
-    "Lunedi" character varying,
-    "Martedi" character varying,
-    "Mercoledi" character varying,
-    "Giovedi" character varying,
-    "Venerdi" character varying,
-    "Sabato" character varying,
-    "Domenica" character varying,
-    "IDtabella_n" integer,
-    "IDdescrizioneEsercizioAllFisForRes" integer,
+    "IDdetAllFisForRes"                     serial NOT NULL,
+    "IDallenamento"                         integer NOT NULL,
+    "IDsettimana"                           integer NOT NULL,
+    "IDseduta"                              integer NOT NULL,
+    "Lunedi"                                character varying,
+    "Martedi"                               character varying,
+    "Mercoledi"                             character varying,
+    "Giovedi"                               character varying,
+    "Venerdi"                               character varying,
+    "Sabato"                                character varying,
+    "Domenica"                              character varying,
+    "IDtabella_n"                           integer,
+    "IDdescrizioneEsercizioAllFisForRes"    integer,
     PRIMARY KEY ("IDdetAllFisForRes")
 );
 
 CREATE TABLE IF NOT EXISTS public."TStabellaNumero"
 (
-    "IDtabella_n" serial NOT NULL,
-    "NumeroTabella" integer,
+    "IDtabella_n"           serial NOT NULL,
+    "NumeroTabella"         integer,
     PRIMARY KEY ("IDtabella_n")
 );
 
 CREATE TABLE IF NOT EXISTS public."TSdescrizioneEsercizioAllFisForRes"
 (
-    "IDdescrizioneEsercizioAllFisForRes" serial NOT NULL,
-    "DescrizioneEsercizio" character varying,
-    "Destext" text,
+    "IDdescrizioneEsercizioAllFisForRes"    serial NOT NULL,
+    "DescrizioneEsercizio"                  character varying,
+    "Destext"                               text,
     PRIMARY KEY ("IDdescrizioneEsercizioAllFisForRes")
 );
 
 CREATE TABLE IF NOT EXISTS public."TdetAllFisCor"
 (
-    "IDdetAllFisCor" serial NOT NULL,
-    "IDallenamento" integer NOT NULL,
-    "IDsettimana" integer NOT NULL,
-    "IDseduta" integer NOT NULL,
-    "IDattrezzo" integer,
-    "Lunedi" character varying,
-    "Martedi" character varying,
-    "Mercoledi" character varying,
-    "Giovedi" character varying,
-    "Venerdi" character varying,
-    "Sabato" character varying,
-    "Domenica" character varying,
-    "IDdescrizioneEsercizioAllFisCor" integer,
+    "IDdetAllFisCor"                    serial NOT NULL,
+    "IDallenamento"                     integer NOT NULL,
+    "IDsettimana"                       integer NOT NULL,
+    "IDseduta"                          integer NOT NULL,
+    "IDattrezzo"                        integer,
+    "Lunedi"                            character varying,
+    "Martedi"                           character varying,
+    "Mercoledi"                         character varying,
+    "Giovedi"                           character varying,
+    "Venerdi"                           character varying,
+    "Sabato"                            character varying,
+    "Domenica"                          character varying,
+    "IDdescrizioneEsercizioAllFisCor"   integer,
     PRIMARY KEY ("IDdetAllFisCor")
 );
 
 CREATE TABLE IF NOT EXISTS public."TSattrezzi"
 (
-    "IDattrezzo" serial NOT NULL,
-    "AttrezzoDes" character varying,
+    "IDattrezzo"                serial NOT NULL,
+    "AttrezzoDes"               character varying,
     PRIMARY KEY ("IDattrezzo")
 );
 
 CREATE TABLE IF NOT EXISTS public."TSdesEsercizioAllFisCor"
 (
-    "IDdescrizioneEsercizioAllFisCor" serial NOT NULL,
-    "DescrizioneEsercizio" character varying,
-    "EsText" text,
+    "IDdescrizioneEsercizioAllFisCor"               serial NOT NULL,
+    "DescrizioneEsercizio"                          character varying,
+    "EsText"                                        text,
     PRIMARY KEY ("IDdescrizioneEsercizioAllFisCor")
 );
 
 CREATE TABLE IF NOT EXISTS public."TdetNoteAtleta"
 (
-    "IDnota"         serial NOT NULL,
-    "IDallenamento"  integer NOT NULL,
-    "IDsettimana"    integer NOT NULL,
-    "Nota"           text,
+    "IDnota"                    serial NOT NULL,
+    "IDallenamento"             integer NOT NULL,
+    "IDsettimana"               integer NOT NULL,
+    "Nota"                      text,
     PRIMARY KEY ("IDnota"),
     FOREIGN KEY ("IDallenamento")
         REFERENCES public."Tallenamenti" ("IDallenamento")
@@ -281,11 +281,11 @@ CREATE TABLE IF NOT EXISTS public."TdetNoteAtleta"
 
 CREATE TABLE IF NOT EXISTS public."Tvisitemediche"
 (
-    "IDvisita"      serial NOT NULL,
-    "IDatleta"      integer NOT NULL,
-    "DataVisita"    date NOT NULL,
-    "DataScadenza"  date NOT NULL,
-    PRIMARY KEY     ("IDvisita")
+    "IDvisita"          serial NOT NULL,
+    "IDatleta"          integer NOT NULL,
+    "DataVisita"        date NOT NULL,
+    "DataScadenza"      date NOT NULL,
+    PRIMARY KEY ("IDvisita")
 );
 
 CREATE TABLE IF NOT EXISTS public."Tantidoping"
@@ -297,6 +297,28 @@ CREATE TABLE IF NOT EXISTS public."Tantidoping"
     "ScadenzaAutorizzazione"    date,
     PRIMARY KEY ("IDantidoping")
 );
+
+CREATE TABLE IF NOT EXISTS public."TSpianogare"
+(
+    "IDtipogara"        serial NOT NULL,
+    "Descrizione"       character varying,
+    "Note"              character varying,
+    PRIMARY KEY ("IDtipogara")
+);
+
+CREATE TABLE IF NOT EXISTS public."Tpianogare"
+(
+    "IDpianogara"               serial NOT NULL,
+    "IDallenamento"             integer NOT NULL,
+    "IDtipogara"                integer,
+    "Data"                      date,
+    "Luogo"                     character varying,
+    "Distanza"                  character varying,
+    "Note"                      character varying,
+    "EscludiVisualizzazione"    BOOLEAN NOT NULL DEFAULT false,
+    PRIMARY KEY ("IDpianogara")
+);
+
 
 -- ==========================================
 -- VINCOLI E CHIAVI ESTERNE (ALTER TABLES)
@@ -370,5 +392,11 @@ ALTER TABLE IF EXISTS public."Tvisitemediche"
 ALTER TABLE IF EXISTS public."Tantidoping"
     ADD FOREIGN KEY ("IDatleta")
     REFERENCES public."Tatleti" ("IDatleta") ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+ALTER TABLE IF EXISTS public."Tpianogare"
+    ADD FOREIGN KEY ("IDallenamento")
+    REFERENCES public."Tallenamenti" ("IDallenamento") ON UPDATE NO ACTION ON DELETE NO ACTION,
+    ADD FOREIGN KEY ("IDtipogara")
+    REFERENCES public."TSpianogare" ("IDtipogara") ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 COMMIT;
